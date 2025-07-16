56a145afe9a386790aa0414e535ca156e66d6383
Joao Breno Gomes <138957799+Jhonnee101@users.noreply.github.com>
2025-07-16 15:25:24 -0300

    add

--- tokens_management/settings.py
+++ tokens_management/settings.py
@@ -110,10 +110,15 @@ WSGI_APPLICATION = 'tokens_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tokens_app',
+        'USER': 'DBA',
+        'PASSWORD': 'HRBCDBA',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
